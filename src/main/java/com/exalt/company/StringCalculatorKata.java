b07720df2c7e7a415729e4f9f45b3c8149ca2aaf
Rezouane <rezouane.m@gmail.com>
2023-01-08 18:24:25 +0100

    green tests (1)

--- src/main/java/com/exalt/company/StringCalculatorKata.java
+++ src/main/java/com/exalt/company/StringCalculatorKata.java
@@ -1,8 +1,24 @@
 package com.exalt.company;
 
+import java.util.Arrays;
+
 public class StringCalculatorKata {
 
     int add(String numbers) {
-        throw new UnsupportedOperationException("Please, complete this method with your implementation");
+        if (numbers == null || numbers.equals("")) {
+            return 0;
+        }
+        String delimiter = ",";
+
+        return Arrays.stream(extractNumbers(splitNumbers(numbers, delimiter))).sum();
+
+    }
+
+    private String[] splitNumbers(String numbers, String delimiter) {
+        return numbers.split(delimiter);
+    }
+
+    private int[] extractNumbers(String[] numbers) {
+        return Arrays.stream(numbers).mapToInt(Integer::parseInt).toArray();
     }
 }
